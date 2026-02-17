7f40ac54ac4989e9c9a9e64c41831b9a2f4fe4a9
zaizai <87618154+ZzaiQWQ@users.noreply.github.com>
2026-02-17 17:22:28 +0800

    Add files via upload

---
+++ photos.js
@@ -0,0 +1,14 @@
+window.MY_ALBUMS = [
+  {
+    id: 'album1',
+    name: '治愈风景 🏔️',   // 相册显示的名字
+    folder: 'travel',     // images 下的文件夹名字
+    files: ['2.jpg', '2.jpg'] // 文件夹里的文件名
+  },
+  {
+    id: 'album2',
+    name: '我的猫咪 🐱',
+    folder: 'cats',
+    files: ['unnamed.jpg', 'b.jpg']
+  }
+];
